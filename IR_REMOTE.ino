f0f2e14d31439df802fb58b592e9791b1559756c
Wojtek <zarzycki.wojciech@icloud.com>
2024-09-29 20:19:06 +0200

    [INIT] FIrst commit

---
+++ IR_REMOTE.ino
@@ -0,0 +1,17 @@
+#include <IRremote.hpp>
+
+const int IR_RECEIVE_PIN = 7;
+
+void setup() {
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+
+}
+
+void loop() {
+  if(IrReceiver.decode()){
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    IrReceiver.printIRResultShort(&Serial);
+    IrReceiver.resume();
+  }
+}
